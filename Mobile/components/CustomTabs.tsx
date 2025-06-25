0b2ad47fb9e660f07b07379d65ca03039881534e
Md. Hasnain <rajon013039799@gmail.com>
2025-06-25 19:44:33 +0600

    Added Custom Tabs

--- Mobile/components/CustomTabs.tsx
+++ Mobile/components/CustomTabs.tsx
@@ -112,8 +112,6 @@ const styles = StyleSheet.create({
     borderRadius: verticalScale(30),
     justifyContent: "center",
     alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 4.65,
     elevation: 8,
