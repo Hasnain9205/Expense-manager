5df7e90265706f982c63aeaa01e7e7b60540a699
Md. Hasnain <rajon013039799@gmail.com>
2025-06-28 06:09:14 +0600

    create getAllIncome,updateIncome and deleteIncome api successfully

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -42,3 +42,57 @@ exports.getAllIncome = async (req, res) => {
     });
   }
 };
+
+//update income
+exports.updateIncome = async (req, res) => {
+  const userId = req.user.id;
+  const incomeId = req.params.id;
+  try {
+    const { amount, category, description, date } = req.body;
+    const updateData = { amount, category, description, date };
+    if (updateData.date) {
+      updateData.date = new Date(updateData.date);
+    }
+    const updatedIncome = await Income.findOneAndUpdate(
+      { _id: incomeId, userId },
+      updateData,
+      { new: true }
+    );
+    if (!updatedIncome) {
+      return res.status(404).json({ message: "Income not found" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Income updated successfully",
+      updatedIncome,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error update income in server",
+      error: error.message,
+    });
+  }
+};
+
+//delete income
+exports.deleteIncome = async (req, res) => {
+  const userId = req.user.id;
+  const incomeId = req.params.id;
+  try {
+    const deleteIncome = await Income.findOneAndDelete({
+      _id: incomeId,
+      userId: userId,
+    });
+    if (!deleteIncome) {
+      return res.status(404).json({ message: "Income not found" });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: "Income deleted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting income in server",
+      error: error.message,
+    });
+  }
+};
--- backend/routes/incomeRoutes.js
+++ backend/routes/incomeRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { addIncome } = require("../controllers/incomeController");
+const {
+  addIncome,
+  getAllIncome,
+  updateIncome,
+  deleteIncome,
+} = require("../controllers/incomeController");
 
 const router = express.Router();
 
 router.post("/add", protect, addIncome);
+router.get("/get-all-income", protect, getAllIncome);
+router.put("/update-income/:id", protect, updateIncome);
+router.delete("/delete-income/:id", protect, deleteIncome);
 
 module.exports = router;
